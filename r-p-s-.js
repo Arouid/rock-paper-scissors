ca3d79d3e683e770bacbbaf8937acda761d49d3d
Arouid <scott77598@outlook.com>
2024-07-01 17:51:27 -0500

    add inituial functions and variables outline

--- r-p-s-.js
+++ r-p-s-.js
@@ -1,13 +1,37 @@
 // assign variables and weights to rock
+let rock;
 // assign variables and weightsb paper
+let paper;
 // assign variables and weights scissors
-// create a way to randomize choice
-// create a way for a player to choose
+let scissors;
+
 // track games played vs scorekeeping 
+// incremnt number based on winner
+let humanScore = 0;
+let computerScore = 0;
 // create two game modes -- single and best of
+// code function to console.log
+function playRound(HumanChoice, computerChoice) {
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 // Initial weights with no logic
 // rock > scissors < paper
 // paper > rock < scissors
 // scissors > paper < rock
-console.log()
+
+// create a way to randomize choice
+function getComputerChoice () {
+
+}
+
+// create a way for a player to choose
+// make case insensitve 
+function getHumanChoice () {
+
+}
