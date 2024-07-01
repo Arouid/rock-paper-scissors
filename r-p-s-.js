80ac1b3a8f822af04a31d7834e483cc033e051b2
Arouid <scott77598@outlook.com>
2024-07-01 17:33:40 -0500

    add pseudocode for rock, paper, scissors. add boilerplate html

---
+++ r-p-s-.js
@@ -0,0 +1,13 @@
+// assign variables and weights to rock
+// assign variables and weightsb paper
+// assign variables and weights scissors
+// create a way to randomize choice
+// create a way for a player to choose
+// track games played vs scorekeeping 
+// create two game modes -- single and best of
+
+// Initial weights with no logic
+// rock > scissors < paper
+// paper > rock < scissors
+// scissors > paper < rock
+console.log()
